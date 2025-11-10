c1928c9ef055acad228e979cbd290cad8717d935
Fredy Leon <fredy.leon@ua.la.com>
2025-11-10 10:25:30 +0100

    Initial Commit

---
+++ fintechKids/fintechKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fintechKidsApp.swift
+//  fintechKids
+//
+//  Created by Fredy Leon on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct fintechKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
