82ab6b99713be3cf212b1840fc2caac1b2351ce8
Fredy Leon <fredy.leon@ua.la.com>
2025-11-15 17:53:56 +0100

    initial commit

--- fintechKids/ContentView.swift
+++ fintechKids/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import FHKUtils
 
 struct ContentView: View {
     var body: some View {
@@ -16,6 +17,9 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear  {
+            Logger.info("Success")
+        }
     }
 }
 
