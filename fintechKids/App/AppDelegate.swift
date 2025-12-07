0d454f0cf8bf6ddae76487396c11f0890478f2be
Fredy Leon <fredy.leon@ua.la.com>
2025-12-07 19:03:55 +0100

    feat: Configuración de CI/CD, .gitignore y cambios de estructura de la rama FHK-1

---
+++ fintechKids/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  fintechKids
+//
+//  Created by Fredy Leon on 7/12/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
--- fintechKids/App/fintechKidsApp.swift
+++ fintechKids/App/fintechKidsApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct fintechKidsApp: App {
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             LanguageView()
