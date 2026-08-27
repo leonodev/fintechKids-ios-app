d900ee2da40b00e352fada6b9952040eb4d43016
Fredy Leon <fredy.leon@ua.la.com>
2026-08-27 15:53:35 +0200

    fix screem

--- FintechHomeKids/Targets/Features/FHKAuth/Sources/Splash/FHKSplashScreen.swift
+++ FintechHomeKids/Targets/Features/FHKAuth/Sources/Splash/FHKSplashScreen.swift
@@ -9,10 +9,12 @@ import SwiftUI
 import FHKDesignSystem
 import FLibUtils
 import FHKTesting
+import FHKCore
 
 public struct FHKSplashScreen: View {
     @State private var viewModel: FHKSplashScreenVM
-    
+    private var router = NavigationRouter<AuthRoute>()
+
     public init() {
         self._viewModel = State(initialValue: FHKSplashScreenVM())
     }
